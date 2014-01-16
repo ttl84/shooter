3fd5089daa05640d0309be6e365414ef453e34d5
ultramailman <cyrus9212@gmail.com>
2014-01-16 00:14:31 -0800

    switched to c++11, added star field, bullets

--- Makefile
+++ Makefile
@@ -11,7 +11,7 @@ endif
 
 LDFLAGS+=-lm\
 	-lSDLmain -lSDL -lSDL_image
-CFLAGS+=-std=c++03\
+CFLAGS+=-std=c++0x\
 	-pedantic-errors -Wstrict-aliasing=0 -Wall\
 	-g\
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,256 @@
+#include <cstdlib>
+#include <cmath>
+#include <deque>
 #include <iostream>
 #include <SDL/SDL.h>
-#include <SDL/SDL_Image.h>
-char const * player_img = "\
-        *        \
-       * *       \
-       * *       \
-      *   *      \
-     *     *     \
-   **       **   \
-**             **\
-   ***     ***   ";
+std::string const player_pixels = "\
+        *        \n\
+       * *       \n\
+       * *       \n\
+      * * *      \n\
+     * * * *     \n\
+   ** *   * **   \n\
+**  * *   * *  **\n\
+   **  ***  **   ";
+
+std::string const bullet_pixels = "\
+*\n\
+*";
+
+struct Vec2{
+	float x, y;
+	Vec2(void): x(0), y(0)
+	{
+	}
+	Vec2(float X, float Y): x(X), y(Y)
+	{
+	}
+};
+
+Vec2 const operator+(Vec2 const & a, Vec2 const & b)
+{
+	Vec2 c;
+	c.x = a.x + b.x;
+	c.y = a.y + b.y;
+	return c;
+}
+Vec2 const & operator+=(Vec2 & a, Vec2 const & b)
+{
+	a.x += b.x;
+	a.y += b.y;
+	return a;
+}
+Vec2 operator*(Vec2 const & a, float k)
+{
+	Vec2 b;
+	b.x = a.x * k;
+	b.y = a.y * k;
+	return b;
+}
+Vec2 operator*(float k, Vec2 const & a)
+{
+	Vec2 b;
+	b.x = a.x * k;
+	b.y = a.y * k;
+	return b;
+}
+
+// Rect represents a rectangle with top left corner as (x, y)
+struct Rect{
+	float x, y;
+	float w, h;
+	
+	Rect(float X, float Y, float W, float H)
+	{
+		if(W < 0)
+		{
+			X -= W;
+			W = -W;
+		}
+		if(H < 0)
+		{
+			Y -= H;
+			H = -H;
+		}
+		x = X;
+		y = Y;
+		w = W;
+		h = H;
+	}
+	Rect(void): x(0), y(0), w(0), h(0)
+	{
+	}
+	Vec2 getCenter(void) const
+	{
+		Vec2 point;
+		point.x = x + w / 2;
+		point.y = y + h / 2;
+		return point;
+	}
+	void setCenter(Vec2 const & center)
+	{
+		x = center.x - w / 2;
+		y = center.y - h / 2;
+	}
+	float getCenterY(void) const
+	{
+		return y + w / 2;
+	}
+	void setCenterY(float center_y)
+	{
+		y = center_y - h / 2;
+	}
+	Vec2 getPosition(void) const
+	{
+		Vec2 point;
+		point.x = x;
+		point.y = y;
+		return point;
+	}
+	float getTop(void) const
+	{
+		return y;
+	}
+	float getBottom(void) const
+	{
+		return y + w;
+	}
+	float getLeft(void) const
+	{
+		return x;
+	}
+	float getRight(void) const
+	{
+		return x + w;
+	}
+	bool contains(Vec2 const & p)const
+	{
+		return p.x >= x && p.x <= (x + w) &&
+			p.y >= y && p.y <= (y + h);
+	}
+};
+
+#define BIT(n) (1 << (n))
+namespace component{
+	enum flag{
+		POSITION = BIT(0),
+		VELOCITY = BIT(1),
+		IMAGE = BIT(2),
+		PLAYER_CONTROL = BIT(3),
+		ENEMY_CONTROL = BIT(4),
+		COLLISION_DAMAGE = BIT(5),
+		COLLISION_EFFECT = BIT(6),
+		LIFESPAN = BIT(7)
+	};
+	unsigned const bullet_mask = component::POSITION | component::VELOCITY | component::IMAGE | component::LIFESPAN;
+	unsigned const move_mask = component::POSITION | component::VELOCITY;
+}
+template <unsigned MAX_ENTITIES>
+struct Entity{
+	unsigned mask[MAX_ENTITIES];
+	Vec2 position[MAX_ENTITIES];
+	Vec2 velocity[MAX_ENTITIES];
+	SDL_Surface * image[MAX_ENTITIES];
+	int collision_damage[MAX_ENTITIES];
+	float lifespan[MAX_ENTITIES];
+	
+	Entity(void): m_count(0)
+	{
+		for(unsigned i = 0; i < MAX_ENTITIES; i++)
+			mask[i] = 0;
+	}
+	unsigned claim(void)
+	{
+		if(holes > 0)
+		{
+			for(unsigned i = 0; i < m_count; i++)
+			{
+				if(mask[i] == 0)
+				{
+					holes--;
+					return i;
+				}
+			}
+		}
+		if(m_count == MAX_ENTITIES)
+			return m_count - 1;
+		else
+			return m_count++;
+	}
+	void remove(unsigned i)
+	{
+		mask[i] = 0;
+		SDL_FreeSurface(image[i]);
+		image[i] = NULL;
+		holes++;
+	}
+	unsigned count(void) const
+	{
+		return m_count;
+	}
+	unsigned capacity(void) const
+	{
+		return MAX_ENTITIES;
+	}
+private:
+	unsigned m_count;
+	unsigned holes;
+};
+
+
+SDL_Surface * create_image(std::string const & pixels, SDL_PixelFormat const & format)
+{
+	unsigned width = 0;
+	for(unsigned i = 0; i < pixels.length(); i++)
+	{
+		if(pixels[i] == '\n')
+			break;
+		else
+			width++;
+	}
+	
+	unsigned height = 1;
+	for(unsigned i = 0; i < pixels.length(); i++)
+	{
+		if(pixels[i] == '\n')
+			height++;
+	}
+	
+	SDL_Surface * surface = SDL_CreateRGBSurface(SDL_SWSURFACE | SDL_SRCALPHA, width, height, 32,
+		format.Rmask, format.Gmask, format.Bmask, format.Amask);
+	if(surface == NULL)
+		return NULL;
+	Uint32 * data = (Uint32*)(surface->pixels);
+	
+	SDL_LockSurface(surface);
+	unsigned i = 0;
+	for(auto iter = pixels.begin(); i < width * height && iter < pixels.end(); ++iter)
+	{
+		if(*iter != '\n')
+		{
+			if(*iter == '*')
+				data[i] = 0xffffffff;
+			else
+				data[i] = 0x00000000;
+			i++;
+		}
+	}
+	SDL_UnlockSurface(surface);
+	
+	return surface;
+}
 bool running = true;
-float const dt = 0.1;
+float const dt_unit = 1.0 /1000.0;
 unsigned window_width = 320;
 unsigned window_height = 320;
 
 SDL_Surface * screen = NULL;
-SDL_Rect camera;
+Rect camera;
 
-unsigned const player = 0;
+unsigned player;
+
+Entity<1000> entities;
+std::deque<Vec2> stars;
 
 namespace control{
 	bool faster = false;
@@ -27,25 +259,10 @@ namespace control{
 	bool right = false;
 	bool fire = false;
 }
-struct vec2{
-	unsigned x, y;
-};
 
-#define MAX_ENTITIES 200
-#define BIT(n) (1 << (n))
-namespace component{
-	enum flag{
-		POSITION = BIT(0),
-		VELOCITY = BIT(1),
-		IMAGE = BIT(2)
-	};
-}
-struct entities{
-	unsigned component_flag[MAX_ENTITIES];
-	vec2 position[MAX_ENTITIES];
-	vec2 velocity[MAX_ENTITIES];
-	SDL_Surface * image[MAX_ENTITIES];
-};
+
+
+
 void handle_event(void)
 {
 	SDL_Event e;
@@ -62,8 +279,12 @@ void handle_event(void)
 				control::left = true;
 			else if(symbol == SDLK_RIGHT)
 				control::right = true;
+			
+			else if(symbol == SDLK_z)
+				control::fire = true;
 			else if(symbol == SDLK_ESCAPE)
 				running = false;
+				
 		}
 		else if(e.type == SDL_KEYUP)
 		{
@@ -83,36 +304,179 @@ void handle_event(void)
 		}
 	}
 }
-void update(void)
+void player_control(void)
+{
+	if(control::faster)
+	{
+		entities.velocity[player].y = -50;
+	}
+	else if(control::slower)
+	{
+		entities.velocity[player].y = -10;
+	}
+	else
+	{
+		entities.velocity[player].y = -20;
+	}
+	
+	if(control::left)
+	{
+		entities.velocity[player].x = -50;
+	}
+	else if(control::right)
+	{
+		entities.velocity[player].x = 50;
+	}
+	else
+	{
+		entities.velocity[player].x = 0;
+	}
+	
+	if(control::fire)
+	{
+		control::fire = false;
+		unsigned bullet = entities.claim();
+		entities.mask[bullet] = component::bullet_mask;
+		entities.position[bullet] = entities.position[player];
+		entities.velocity[bullet] = entities.velocity[player] + Vec2(0, -50);
+		entities.image[bullet] = create_image(bullet_pixels, *screen->format);
+		entities.lifespan[bullet] = 5.0;
+	}
+}
+void bullet_process(float dt)
+{
+	for(unsigned i = 0; i < entities.count(); i++)
+	{
+		if((entities.mask[i] & component::bullet_mask) == component::bullet_mask)
+		{
+			entities.lifespan[i] -= dt;
+			if(entities.lifespan[i] < 0)
+			{
+				entities.remove(i);
+			}
+		}
+	}
+}
+void move(float dt)
 {
 	
+	for(unsigned i = 0; i < entities.count(); i++)
+	{
+		if((entities.mask[i] & component::move_mask) == component::move_mask)
+		{
+			entities.position[i] += dt * entities.velocity[i];
+		}
+	}
+}
+void update(float const dt_unit)
+{
+	player_control();
+	bullet_process(dt_unit);
+	move(dt_unit);
+	camera.setCenterY(floor(entities.position[player].y));
+	
+	if((rand() % 100) < 10)
+	{
+		Vec2 star;
+		star.y = entities.position[player].y - window_height / 2;
+		star.x = rand() % window_width;
+		stars.push_back(star);
+	}
+	while(stars.front().y > camera.getBottom())
+	{
+		stars.pop_front();
+	}
 }
 void draw(void)
 {
 	SDL_FillRect(screen, NULL, 0x00000000);
+	
+	// draw entities (space fighters)
+	unsigned draw_mask = component::POSITION | component::IMAGE;
+	for(unsigned i = 0; i < entities.count(); i++)
+	{
+		if((entities.mask[i] & draw_mask) == draw_mask)
+		{
+			SDL_Rect position;
+			position.x = entities.position[i].x - camera.x;
+			position.y = entities.position[i].y - camera.y;
+			SDL_BlitSurface(entities.image[i], NULL, screen, &position);
+		}
+	}
+	
+	// draw stars (background)
+	SDL_LockSurface(screen);
+	for(auto i = stars.begin(), end = stars.end(); i < end; ++i)
+	{
+		unsigned x = i->x - camera.x;
+		unsigned y = i->y - camera.y;
+		if(x >= 0 && x < screen->w && y >= 0 && y < screen->h)
+			((Uint32*)(screen->pixels))[y * screen->w + x] = 0x7f7f7fff;
+	}
+	SDL_UnlockSurface(screen);
 	SDL_Flip(screen);
 }
-int main(int argc, char ** argv)
+void init_system(void)
 {
 	SDL_InitSubSystem(SDL_INIT_VIDEO |
 		SDL_INIT_EVENTTHREAD |
 		SDL_INIT_TIMER);
 	screen = SDL_SetVideoMode(
 		window_width, window_height, 32, SDL_SWSURFACE);
+}
+void init_camera(void)
+{
 	camera.x = 0;
 	camera.y = 0;
 	camera.w = window_width;
 	camera.h = window_height;
+}
+
+void init_player(void)
+{
+	player = entities.claim();
+	entities.mask[player] =
+		component::POSITION |
+		component::VELOCITY |
+		component::IMAGE |
+		component::PLAYER_CONTROL;
+	entities.image[player] = create_image(player_pixels, *(screen->format));
 	
-	while(running)
-	{
-		handle_event();
-		update();
-		draw();
-	}
+	Rect player_rect(0, 0, entities.image[player]->w, entities.image[player]->h);
+	player_rect.setCenter(camera.getCenter());
+	entities.position[player] = player_rect.getPosition();
+}
+void cleanup_system(void)
+{
 	SDL_QuitSubSystem(SDL_INIT_VIDEO |
 		SDL_INIT_EVENTTHREAD |
 		SDL_INIT_TIMER);
 	SDL_Quit();
+}
+int main(int argc, char ** argv)
+{
+	init_system();
+	init_camera();
+	
+	init_player();
+	
+	Uint32 frame_begin = 0, frame_end = 0;
+	float dt = 0.017;
+	while(running)
+	{
+		frame_begin = frame_end;
+		
+		handle_event();
+		while(dt > 0)
+		{
+			update(dt_unit);
+			dt -= dt_unit;
+		}
+		draw();
+		
+		frame_end = SDL_GetTicks();
+		dt += (float)(frame_end - frame_begin) / 1000.0;
+	}
+	cleanup_system();
 	return 0;
 }
