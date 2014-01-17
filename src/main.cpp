46f05fc1b162851e6020b765b1c5fd8d9ba3fa8a
ultramailman <cyrus9212@gmail.com>
2014-01-16 23:25:12 -0800

    added enemies

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,48 @@
 #include <cstdlib>
 #include <cmath>
 #include <cstdint>
+#include <vector>
 #include <deque>
+#include <map>
+#include <stack>
 #include <iostream>
 #include <SDL/SDL.h>
 std::string const player_pixels = "\
         *        \n\
+        *        \n\
+       * *       \n\
+       * *       \n\
        * *       \n\
        * *       \n\
       * * *      \n\
+      * * *      \n\
+     * * * *     \n\
      * * * *     \n\
    ** *   * **   \n\
+   ** *   * **   \n\
 **  * *   * *  **\n\
+**  * *   * *  **\n\
+   **  ***  **   \n\
    **  ***  **   ";
 
+std::string const enemy_pixels = "\
+        *        \n\
+        *        \n\
+       * *       \n\
+      ** **      \n\
+     *** ***     \n\
+      ** **      \n\
+       * *       \n\
+      ** **      \n\
+    **** ****    \n\
+   ***** *****   \n\
+  *** * * * ***  \n\
+ **** * * * **** \n\
+***    ***    ***\n\
+**     ***     **\n\
+ *      *      * \n\
+ *      *      * \n\
+        *        ";
 std::string const bullet_pixels = "\
 *\n\
 *";
@@ -133,89 +162,101 @@ struct Rect{
 	}
 };
 struct Gun{
+	bool fire;
 	float wait_time;
 	float delay;
-	enum firing_side{
+	enum class Side{
 		CENTER,
 		LEFT,
 		RIGHT
 	}side;
-	bool fire;
-	unsigned parent;
+	Vec2 direction;
+	float bullet_speed;
 };
-#define BIT(n) (1 << (n))
-typedef uint16_t mask_t;
-namespace component{
+enum class Faction{
+	PLAYER,
+	ENEMY
+};
+
+namespace ecs{
+	typedef uint16_t mask_t;
+	typedef unsigned entity_t;
+	unsigned constexpr BIT(unsigned n)
+	{
+		return 1 << n;
+	}
 	enum flag{
 		POSITION = BIT(0),
 		VELOCITY = BIT(1),
 		IMAGE = BIT(2),
-		PLAYER_CONTROL = BIT(3),
+		HEALTH = BIT(3),
 		ENEMY_CONTROL = BIT(4),
 		COLLISION_DAMAGE = BIT(5),
-		COLLISION_EFFECT = BIT(6),
-		LIFESPAN = BIT(7),
-		GUN = BIT(8),
-		SIZE = BIT(9)
+		LIFESPAN = BIT(6),
+		GUN = BIT(7),
+		SIZE = BIT(8),
+		FACTION = BIT(9)
 	};
-	mask_t const player_mask = POSITION | VELOCITY | SIZE | IMAGE | PLAYER_CONTROL | GUN;
-	mask_t const bullet_mask = POSITION | VELOCITY | SIZE | IMAGE | LIFESPAN;
-	mask_t const move_mask = POSITION | VELOCITY;
-	mask_t const shooter_mask = POSITION | SIZE | GUN;
-}
-template <unsigned MAX_ENTITIES>
-struct Entity{
-	mask_t mask[MAX_ENTITIES];
-	Vec2 position[MAX_ENTITIES];
-	Vec2 velocity[MAX_ENTITIES];
-	Size size[MAX_ENTITIES];
-	SDL_Surface * image[MAX_ENTITIES];
-	int collision_damage[MAX_ENTITIES];
-	float lifespan[MAX_ENTITIES];
-	Gun gun[MAX_ENTITIES];
+	mask_t constexpr move_mask = POSITION | VELOCITY;
+	mask_t constexpr shooter_mask = POSITION | SIZE | GUN;
+	mask_t constexpr player_mask = move_mask | shooter_mask | IMAGE | HEALTH | COLLISION_DAMAGE | FACTION;
+	mask_t constexpr enemy_mask = move_mask | shooter_mask | IMAGE | ENEMY_CONTROL | HEALTH | COLLISION_DAMAGE | FACTION;
+	mask_t constexpr bullet_mask = POSITION | VELOCITY | SIZE | IMAGE | LIFESPAN | COLLISION_DAMAGE | FACTION;
 	
-	Entity(void): m_count(0)
-	{
-		for(unsigned i = 0; i < MAX_ENTITIES; i++)
-			mask[i] = 0;
-	}
-	unsigned claim(void)
-	{
-		if(holes > 0)
+	template <unsigned MAX_ENTITIES>
+	struct Entity{
+		mask_t mask[MAX_ENTITIES];
+		Vec2 position[MAX_ENTITIES];
+		Vec2 velocity[MAX_ENTITIES];
+		Size size[MAX_ENTITIES];
+		SDL_Surface * image[MAX_ENTITIES];
+		int collision_damage[MAX_ENTITIES];
+		float lifespan[MAX_ENTITIES];
+		unsigned life[MAX_ENTITIES];
+		Gun gun[MAX_ENTITIES];
+		
+		Entity(void): mask({0}), image({NULL}), m_count(0)
 		{
-			for(unsigned i = 0; i < m_count; i++)
+		}
+		entity_t claim(void)
+		{
+			
+			if(holes.empty())
 			{
-				if(mask[i] == 0)
-				{
-					holes--;
-					return i;
-				}
+				if(m_count == MAX_ENTITIES)
+					return m_count - 1;
+				else
+					return m_count++;
+			}
+			else
+			{
+				entity_t hole = holes.top();
+				holes.pop();
+				return hole;
 			}
 		}
-		if(m_count == MAX_ENTITIES)
-			return m_count - 1;
-		else
-			return m_count++;
-	}
-	void remove(unsigned i)
-	{
-		mask[i] = 0;
-		SDL_FreeSurface(image[i]);
-		image[i] = NULL;
-		holes++;
-	}
-	unsigned count(void) const
-	{
-		return m_count;
-	}
-	unsigned capacity(void) const
-	{
-		return MAX_ENTITIES;
-	}
-private:
-	unsigned m_count;
-	unsigned holes;
-};
+		void remove(entity_t i)
+		{
+			mask[i] = 0;
+			SDL_FreeSurface(image[i]);
+			image[i] = NULL;
+			holes.push(i);
+		}
+		unsigned count(void) const
+		{
+			return m_count;
+		}
+		unsigned capacity(void) const
+		{
+			return MAX_ENTITIES;
+		}
+	private:
+		unsigned m_count;
+		std::stack<entity_t> holes;
+	};
+
+}
+
 
 
 SDL_Surface * surface_from_string(std::string const & pixels, SDL_PixelFormat const * format)
@@ -236,28 +277,45 @@ SDL_Surface * surface_from_string(std::string const & pixels, SDL_PixelFormat co
 			height++;
 	}
 	
-	SDL_Surface * surface = SDL_CreateRGBSurface(SDL_SWSURFACE | SDL_SRCALPHA, width, height, 32,
+	SDL_Surface * surface = SDL_CreateRGBSurface(SDL_SWSURFACE, width, height, 32,
 		format->Rmask, format->Gmask, format->Bmask, format->Amask);
+	SDL_SetColorKey(surface, SDL_SRCCOLORKEY, SDL_MapRGB(surface->format, 255, 0, 0));
+	//SDL_SetAlpha(surface, SDL_SRCALPHA,255);
 	if(surface == NULL)
 		return NULL;
 	Uint32 * data = (Uint32*)(surface->pixels);
 	
 	SDL_LockSurface(surface);
 	unsigned i = 0;
-	for(auto iter = pixels.begin(); i < width * height && iter < pixels.end(); ++iter)
+	for(auto iter = pixels.begin(); i < width * height && iter != pixels.end(); ++iter)
 	{
 		if(*iter != '\n')
 		{
 			if(*iter == '*')
-				data[i] = 0xffffffff;
+				data[i] = SDL_MapRGB(format, 0, 255, 255);
 			else
-				data[i] = 0x00000000;
+				data[i] = SDL_MapRGB(format, 255, 0, 0);
 			i++;
 		}
 	}
 	SDL_UnlockSurface(surface);
 	
 	return surface;
+}
+SDL_Surface * surface_rotate_180(SDL_Surface const * surface)
+{
+	SDL_PixelFormat const * format = surface->format;
+	SDL_Surface * rotated = SDL_CreateRGBSurface(SDL_SWSURFACE | SDL_SRCALPHA, surface->w, surface->h, 32,
+		format->Rmask, format->Gmask, format->Bmask, format->Amask);
+	SDL_SetColorKey(rotated, SDL_SRCCOLORKEY, SDL_MapRGB(rotated->format, 255, 0, 0));
+	
+	SDL_LockSurface(rotated);
+	for(unsigned i = 0, end = surface->w * surface->h; i < end; i++)
+	{
+		((Uint32*)(rotated->pixels))[i] = ((Uint32*)(surface->pixels))[end-i-1];
+	}
+	SDL_UnlockSurface(rotated);
+	return rotated;
 }
 bool running = true;
 float const dt_unit = 1.0 /1000.0;
@@ -267,10 +325,13 @@ unsigned window_height = 320;
 SDL_Surface * screen = NULL;
 Rect camera;
 
-unsigned player;
+ecs::entity_t player;
 
-Entity<1000> entities;
+ecs::Entity<1000> entities;
 std::deque<Vec2> stars;
+
+std::map<std::string, SDL_Surface*> surface_cache;
+
 
 namespace control{
 	bool faster = false;
@@ -366,7 +427,7 @@ void shooter_process(float dt)
 {
 	for(unsigned i = 0; i < entities.count(); i++)
 	{
-		if((entities.mask[i] & component::shooter_mask) == component::shooter_mask)
+		if((entities.mask[i] & ecs::shooter_mask) == ecs::shooter_mask)
 		{
 			if(entities.gun[i].wait_time > 0)
 				entities.gun[i].wait_time -= dt;
@@ -375,25 +436,24 @@ void shooter_process(float dt)
 				entities.gun[i].wait_time += entities.gun[i].delay;
 				
 				unsigned bullet = entities.claim();
-				entities.mask[bullet] = component::bullet_mask;
+				entities.mask[bullet] = ecs::bullet_mask;
 				
-				unsigned parent = entities.gun[i].parent;
-				if(entities.gun[i].side == Gun::LEFT)
+				if(entities.gun[i].side == Gun::Side::LEFT)
 				{
-					entities.position[bullet] = entities.position[parent];
-					entities.gun[i].side = Gun::RIGHT;
+					entities.position[bullet] = entities.position[i];
+					entities.gun[i].side = Gun::Side::RIGHT;
 				}
-				else if(entities.gun[i].side == Gun::RIGHT)
+				else if(entities.gun[i].side == Gun::Side::RIGHT)
 				{
-					entities.position[bullet] = entities.position[parent] + Vec2(entities.size[parent].w, 0);
-					entities.gun[i].side = Gun::LEFT;
+					entities.position[bullet] = entities.position[i] + Vec2(entities.size[i].w, 0);
+					entities.gun[i].side = Gun::Side::LEFT;
 				}
-				else
+				else if(entities.gun[i].side == Gun::Side::CENTER)
 				{
-					entities.position[bullet] = entities.position[parent] + Vec2(entities.size[parent].w / 2, 0);
+					entities.position[bullet] = entities.position[i] + Vec2(entities.size[i].w / 2, 0);
 				}
 				
-				entities.velocity[bullet] = entities.velocity[player] + Vec2(0, -50);
+				entities.velocity[bullet] = entities.velocity[i] + entities.gun[i].bullet_speed * entities.gun[i].direction;
 				entities.image[bullet] = surface_from_string(bullet_pixels, screen->format);
 				entities.lifespan[bullet] = 5.0;
 			}
@@ -404,7 +464,7 @@ void bullet_process(float dt)
 {
 	for(unsigned i = 0; i < entities.count(); i++)
 	{
-		if((entities.mask[i] & component::bullet_mask) == component::bullet_mask)
+		if((entities.mask[i] & ecs::bullet_mask) == ecs::bullet_mask)
 		{
 			entities.lifespan[i] -= dt;
 			if(entities.lifespan[i] < 0)
@@ -414,25 +474,82 @@ void bullet_process(float dt)
 		}
 	}
 }
-void move(float dt)
+void move_process(float dt)
 {
 	
 	for(unsigned i = 0; i < entities.count(); i++)
 	{
-		if((entities.mask[i] & component::move_mask) == component::move_mask)
+		if((entities.mask[i] & ecs::move_mask) == ecs::move_mask)
 		{
 			entities.position[i] += dt * entities.velocity[i];
 		}
 	}
 }
-void update(float const dt_unit)
+void update_camera(void)
 {
-	player_control();
-	shooter_process(dt_unit);
-	bullet_process(dt_unit);
-	move(dt_unit);
 	camera.setCenterY(floor(entities.position[player].y));
+}
+void spawn_player(void)
+{
+	player = entities.claim();
 	
+	entities.mask[player] = ecs::player_mask;
+	
+	entities.image[player] = surface_from_string(player_pixels, screen->format);
+	
+	Rect player_rect(0, 0, entities.image[player]->w, entities.image[player]->h);
+	player_rect.setCenter(camera.getCenter());
+	entities.position[player] = player_rect.getPosition();
+	
+	entities.size[player] = Size(entities.image[player]->w, entities.image[player]->h);
+	
+	entities.gun[player].side = Gun::Side::LEFT;
+	entities.gun[player].delay = 0.1;
+	entities.gun[player].direction = Vec2(0, -1);
+	entities.gun[player].bullet_speed = 50;
+	
+	entities.life[player] = 3;
+}
+void spawn_enemy(void)
+{
+	if((rand() % 1000)  == 0)
+	{
+		ecs::entity_t enemy = entities.claim();
+		
+		entities.mask[enemy] = ecs::enemy_mask;
+		
+		SDL_Surface * enemy_image = surface_from_string(enemy_pixels, screen->format);
+		entities.image[enemy] = surface_rotate_180(enemy_image);
+		SDL_FreeSurface(enemy_image);
+		
+		entities.position[enemy] = Vec2(rand() % window_width, camera.getTop() - entities.image[enemy]->h);
+		
+		entities.velocity[enemy] = Vec2(0, rand() % 50 + 1);
+		
+		entities.size[enemy] = Size(entities.image[enemy]->w, entities.image[enemy]->h);
+		
+		entities.gun[enemy].side = Gun::Side::CENTER;
+		entities.gun[enemy].delay = (float)(rand() % 10) + 0.1;
+		entities.gun[enemy].fire = true;
+		entities.gun[enemy].direction = Vec2(0, 1);
+		entities.gun[enemy].bullet_speed = 50;
+		
+		entities.life[enemy] = 10;
+	}
+}
+void despawn_enemy(void)
+{
+	for(unsigned i = 0; i < entities.count(); i++)
+	{
+		if((entities.mask[i] & ecs::enemy_mask) == ecs::enemy_mask)
+		{
+			if(entities.position[i].y > camera.getBottom())
+				entities.remove(i);
+		}
+	}
+}
+void spawn_star(void)
+{
 	if((rand() % 1000) < 10)
 	{
 		Vec2 star;
@@ -440,10 +557,26 @@ void update(float const dt_unit)
 		star.x = rand() % window_width;
 		stars.push_back(star);
 	}
+}
+void despawn_star(void)
+{
 	while(stars.front().y > camera.getBottom())
 	{
 		stars.pop_front();
 	}
+}
+void update(float const dt)
+{
+	player_control();
+	shooter_process(dt);
+	bullet_process(dt);
+	move_process(dt);
+	update_camera();
+	spawn_enemy();
+	despawn_enemy();
+	spawn_star();
+	despawn_star();
+	
 }
 void draw(void)
 {
@@ -451,17 +584,17 @@ void draw(void)
 	
 	// draw stars (background)
 	SDL_LockSurface(screen);
-	for(auto i = stars.begin(), end = stars.end(); i < end; ++i)
+	for(auto i = stars.begin(); i != stars.end(); ++i)
 	{
-		unsigned x = i->x - camera.x;
-		unsigned y = i->y - camera.y;
+		int x = i->x - camera.x;
+		int y = i->y - camera.y;
 		if(x >= 0 && x < screen->w && y >= 0 && y < screen->h)
 			((Uint32*)(screen->pixels))[y * screen->w + x] = SDL_MapRGB(screen->format, 128, 128, 128);
 	}
 	SDL_UnlockSurface(screen);
 	
 	// draw entities (space fighters)
-	unsigned draw_mask = component::POSITION | component::IMAGE;
+	unsigned draw_mask = ecs::POSITION | ecs::IMAGE;
 	for(unsigned i = 0; i < entities.count(); i++)
 	{
 		if((entities.mask[i] & draw_mask) == draw_mask)
@@ -491,24 +624,7 @@ void init_camera(void)
 	camera.h = window_height;
 }
 
-void init_player(void)
-{
-	player = entities.claim();
-	
-	entities.mask[player] = component::player_mask;
-	
-	entities.image[player] = surface_from_string(player_pixels, screen->format);
-	
-	Rect player_rect(0, 0, entities.image[player]->w, entities.image[player]->h);
-	player_rect.setCenter(camera.getCenter());
-	entities.position[player] = player_rect.getPosition();
-	
-	entities.size[player] = Size(entities.image[player]->w, entities.image[player]->h);
-	
-	entities.gun[player].parent = player;
-	entities.gun[player].side = Gun::LEFT;
-	entities.gun[player].delay = 0.1;
-}
+
 void cleanup_system(void)
 {
 	SDL_QuitSubSystem(SDL_INIT_VIDEO |
@@ -521,7 +637,7 @@ int main(int argc, char ** argv)
 	init_system();
 	init_camera();
 	
-	init_player();
+	spawn_player();
 	
 	Uint32 frame_begin = 0, frame_end = 0;
 	float dt = 0.017;
