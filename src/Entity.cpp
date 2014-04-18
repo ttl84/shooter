64acf344fcb1b0847fb55f455a0f83819b134d08
ultramailman <cyrus9212@gmail.com>
2014-04-18 13:35:25 -0700

    new way of using ecs

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -44,4 +44,62 @@ unsigned Entity::capacity(void) const
 	return MAX_ENTITIES;
 }
 }
-#include "List.h"
+
+namespace ecs3{
+	World::World(unsigned Size) :masks(Size), size(Size)
+	{
+		for(unsigned i = size; i != 0; i--)
+			freelist.push(i);
+	}
+	template<class T>
+	void World::addComponent(Component cpn)
+	{
+		components.emplace(cpn, new ComponentList<T>(size));
+	}
+	template<class T>
+	ComponentBase * World::getComponent(Component cpn)
+	{
+		auto it = components.find(cpn);
+		if(it != components.end())
+			return static_cast<T>(it->second.get());
+		else
+			return nullptr;
+	}
+	
+	unsigned World::slotsLeft() const
+	{
+		return freelist.size();
+	}
+	unsigned World::claim()
+	{
+		if(not freelist.empty())
+		{
+			unsigned next = freelist.top();
+			freelist.pop();
+			return next;
+		}
+		else
+		{
+			std::cerr << __func__ << ": no more slots to claim" << std::endl;
+			abort();
+		}
+	}
+	void World::relinquish(unsigned i)
+	{
+		if(freelist.size() >= this->size)
+		{
+			std::cerr << __func__ << ": no more slots to relinquish" << std::endl;
+			abort();
+		}
+		else if(i >= size)
+		{
+			std::cerr << __func__ << ": slot is out of range" << std::endl;
+			abort();
+		}
+		else
+		{
+			masks[i] = 0;
+			freelist.push(i);
+		}
+	}
+}
