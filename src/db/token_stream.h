bc4eeb8dbbaddd1e248032893769ff3677635a9d
ultramailman <cyrus9212@gmail.com>
2015-08-26 23:59:56 -0700

    improved tokenizer. will use it in db soon

--- src/db/token_stream.h
+++ src/db/token_stream.h
@@ -2,7 +2,7 @@
 #include <string>
 
 enum Type{
-	word, col, row, err
+	none, word, col, row, err
 };
 struct Token{
 	Type type;
@@ -35,36 +35,60 @@ public:
 		}
 	TokenIterator& operator++()
 		{
+			tok.type = Type::none;
 			tok.clear();
-			unsigned count = 0;
+			bool quote = false;
+			bool escape = false;
 			while(it != end){
 				char c = *it;
-				if(c == ' ' || c == '\t' || c == '\n') {
+				if(escape) {
+					escape = false;
+					tok.lexeme += c;
 					++it;
-				} else if(c == ',') {
-					if(count == 0) {
-						tok.type = Type::col;
-						tok.lexeme.clear();
+				} else if(quote) {
+					if(c == '\'' || c == '\"') {
+						quote = false;
 						++it;
-						++count;
-					}
-					break;
-				} else if(c == ';') {
-					if(count == 0) {
-						tok.type = Type::row;
-						tok.lexeme.clear();
+						break;
+					} else if(c == '\\') {
+						escape = true;
+					} else {
+						tok.lexeme += c;
 						++it;
-						++count;
 					}
-					break;
 				} else {
-					tok.type = Type::word;
-					tok.lexeme.push_back(c);
-					++it;
-					++count;
+					if(c == '\'' || c == '\"') {
+						tok.type = Type::word;
+						quote = true;
+						++it;
+					} else if(c == ' ' || c == '\t' || c == '\n' || c == '\r') {
+						++it;
+					} else if(c == ',') {
+						if(tok.type == Type::none) {
+							tok.type = Type::col;
+							++it;
+						}
+						break;
+					} else if(c == ';') {
+						if(tok.type == Type::none) {
+							tok.type = Type::row;
+							++it;
+						}
+						break;
+					} else {
+						tok.type = Type::word;
+						tok.lexeme += c;
+						++it;
+					}
 				}
 			}
-			if(count == 0) {
+
+
+			if(quote) {
+				// if quote is still on, then the end has been reached without closing it
+				tok.type = Type::err;
+			} else if(tok.type == Type::none) {
+				// if type is still none after all that, then the end has been reached
 				done = true;
 			}
 			return *this;
